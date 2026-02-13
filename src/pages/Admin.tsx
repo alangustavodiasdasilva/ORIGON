6a1d270367d4cf0f9b5d07e1e0aac1944db83cc9
Seu Nome <seu.email@exemplo.com>
2026-02-13 16:08:49 -0300

    Update last update timestamp

--- src/pages/Admin.tsx
+++ src/pages/Admin.tsx
@@ -185,7 +185,7 @@ function DashboardTab({ onlineAnalysts }: { onlineAnalysts: Analista[] }) {
                         <p className="pt-2 text-neutral-500 border-t border-white/10 mt-2">
                             Sistema de Análise HVI Avançada - Versão 3.1
                             <br />
-                            Última atualização: 12/02/2026 21:59
+                            Última atualização: 13/02/2026 15:44
                             <br />
                             Todos os direitos reservados.
                         </p>
