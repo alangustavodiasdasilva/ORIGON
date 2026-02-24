7704462f2bc9bb946acf2926d837e091c572d0a1
Seu Nome <seu.email@exemplo.com>
2026-02-24 13:46:38 -0300

    fix: adiciona plugin no vite.config para criar 404.html e arrumar rotas no github pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,4 @@
+import fs from 'fs'
 import path from "path"
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
@@ -9,6 +10,15 @@ export default defineConfig({
   plugins: [
     react(),
     tailwindcss(),
+    {
+      name: 'copy-404',
+      writeBundle() {
+        fs.copyFileSync(
+          path.resolve(__dirname, 'dist/index.html'),
+          path.resolve(__dirname, 'dist/404.html')
+        );
+      }
+    }
   ],
   resolve: {
     alias: {
