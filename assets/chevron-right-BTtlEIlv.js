9addbb6cfb7e76375ad9e2efe0d0d1a7b15d760a
Seu Nome <seu.email@exemplo.com>
2026-04-07 23:42:57 -0300

    Updates

--- assets/chevron-right-BTtlEIlv.js
+++
@@ -1,1 +0,0 @@
-import{c as o}from"./index-D6yUu7ni.js";const t=[["path",{d:"m9 18 6-6-6-6",key:"mthhwq"}]],e=o("chevron-right",t);export{e as C};
--- assets/index-vZkbkDQI.js
+++
@@ -1,1 +0,0 @@
-import{x as r}from"./index-D6yUu7ni.js";var a=r();export{a as r};
